15c7194d2f714062c2c3046da7112390531c9e94
killergti <ivan.fedorenkov@gmail.com>
2015-06-23 19:17:50 +0300

    [GTC-3] Detached house property page

--- webapp/src/main/java/org/gotocy/controllers/PropertiesController.java
+++ webapp/src/main/java/org/gotocy/controllers/PropertiesController.java
@@ -5,17 +5,19 @@ import org.gotocy.domain.Property;
 import org.gotocy.repository.LocalizedPropertyRepository;
 import org.gotocy.repository.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Locale;
 
 /**
  * @author ifedorenkov
  */
-@RestController
+@Controller
 public class PropertiesController {
 
 	@Autowired
@@ -26,6 +28,7 @@ public class PropertiesController {
 
 
 	@RequestMapping("/properties/add")
+	@ResponseBody
 	public LocalizedProperty addProperty(@RequestParam(required = false) Long id, Locale locale) {
 		Property p;
 		if (id == null) {
@@ -45,13 +48,15 @@ public class PropertiesController {
 	}
 
 	@RequestMapping("/properties")
+	@ResponseBody
 	public Iterable<LocalizedProperty> list() {
 		return localizedPropertyRepository.findAll();
 	}
 
 	@RequestMapping("/property/{id}")
-	public LocalizedProperty get(@PathVariable("id") LocalizedProperty localizedProperty) {
-		return localizedProperty;
+	public String get(Model model, @PathVariable("id") LocalizedProperty localizedProperty) {
+		model.addAttribute(localizedProperty);
+		return "property/show";
 	}
 
 }
