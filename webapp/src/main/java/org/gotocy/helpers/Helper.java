b8f38779f879248da03ab69feb8fa40f959e8834
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2015-06-27 11:05:26 +0300

    [GTC-3] Detached house property page

--- webapp/src/main/java/org/gotocy/helpers/Helper.java
+++ webapp/src/main/java/org/gotocy/helpers/Helper.java
@@ -1,8 +1,13 @@
 package org.gotocy.helpers;
 
+import org.gotocy.domain.BaseEntity;
+import org.gotocy.domain.LocalizedProperty;
+import org.gotocy.domain.Property;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.thymeleaf.util.NumberPointType;
 import org.thymeleaf.util.NumberUtils;
+
+import java.util.Objects;
 
 /**
  * A helper object for the view layer. Contains a number of utility methods, such as price formatting, etc.
@@ -16,6 +21,38 @@ public class Helper {
 	 */
 	public static String price(int price) {
 		return "$ " + NumberUtils.format(price, 1, NumberPointType.COMMA, LocaleContextHolder.getLocale());
+	}
+
+	/**
+	 * Returns path of a given entity object.
+	 */
+	public static <T extends BaseEntity> String path(T entity) {
+		if (entity instanceof Property) {
+			return "/property/" + entity.getId();
+		} else if (entity instanceof LocalizedProperty) {
+			LocalizedProperty lp = (LocalizedProperty) entity;
+			return getPrefixForLanguage(lp.getLocale()) + path(lp.getProperty());
+		}
+		// TODO: log error
+		return "";
+	}
+
+	/**
+	 * Returns path of of a given entity object using the given language.
+	 * The language should follow {@link java.util.Locale} rules.
+	 */
+	public static <T extends BaseEntity> String path(T entity, String language) {
+		if (entity instanceof Property) {
+			return getPrefixForLanguage(language) + "/property/" + entity.getId();
+		} else if (entity instanceof LocalizedProperty) {
+			return path(((LocalizedProperty) entity).getProperty(), language);
+		}
+		// TODO: log error
+		return "";
+	}
+
+	private static String getPrefixForLanguage(String language) {
+		return Objects.equals(language, "ru") ? "/ru" : "";
 	}
 
 }
--- webapp/src/test/java/org/gotocy/HelperTest.java
+++ webapp/src/test/java/org/gotocy/HelperTest.java
@@ -1,5 +1,7 @@
 package org.gotocy;
 
+import org.gotocy.domain.LocalizedProperty;
+import org.gotocy.domain.Property;
 import org.gotocy.helpers.Helper;
 import org.junit.Assert;
 import org.junit.Test;
@@ -16,5 +18,40 @@ public class HelperTest {
 		Assert.assertEquals("$ 100,000", Helper.price(100000));
 		Assert.assertEquals("$ 100,000,000", Helper.price(100000000));
 	}
+
+	@Test
+	public void url() {
+		Property p = new Property();
+		p.setId(1L);
+		LocalizedProperty lp = new LocalizedProperty();
+		lp.setProperty(p);
+
+		// Language is not specified
+
+		// Just an ordinary path
+		Assert.assertEquals("/property/1", Helper.path(p));
+
+		// Path of LocalizedProperty uses id of parent Property object, adding the language prefix (like localized path
+		// on a Property object).
+		lp.setLocale("en");
+		Assert.assertEquals("/property/1", Helper.path(lp));
+		lp.setLocale("ru");
+		Assert.assertEquals("/ru/property/1", Helper.path(lp));
+
+		// Language is specified explicitly
+
+		// Default language, no prefix
+		Assert.assertEquals("/property/1", Helper.path(p, "en"));
+		// Russian language prefix
+		Assert.assertEquals("/ru/property/1", Helper.path(p, "ru"));
+
+		// The given language overrides LocalizedProperty's locale language
+		lp.setLocale("ru");
+		Assert.assertEquals("/property/1", Helper.path(lp, "en"));
+		lp.setLocale("en");
+		Assert.assertEquals("/ru/property/1", Helper.path(lp, "ru"));
+
+	}
+
 
 }
