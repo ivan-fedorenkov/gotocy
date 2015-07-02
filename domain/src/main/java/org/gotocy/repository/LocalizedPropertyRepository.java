b4cc8ca26581ba51bf9b99bbcecbf7ee7f8d4e21
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2015-07-03 01:41:36 +0300

    [GTC-2] Property pages

--- domain/src/main/java/org/gotocy/repository/LocalizedPropertyRepository.java
+++ domain/src/main/java/org/gotocy/repository/LocalizedPropertyRepository.java
@@ -1,18 +1,29 @@
 package org.gotocy.repository;
 
 import org.gotocy.domain.LocalizedProperty;
+import org.gotocy.domain.Location;
 import org.gotocy.domain.PropertyStatus;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 /**
+ * TODO: ololo auto-generated JPA repo ... n+1 query on each method - no comments
  * @author ifedorenkov
  */
 public interface LocalizedPropertyRepository extends JpaRepository<LocalizedProperty, Long> {
 
-	LocalizedProperty findByPropertyIdAndLocale(Long propertyId, String locale);
+	@Query("select lp from LocalizedProperty lp join fetch lp.property join fetch lp.property.imageSet.representativeImage " +
+		"where lp.property.id = ?1 and lp.locale = ?2")
+	LocalizedProperty findProperty(Long propertyId, String locale);
 
 	List<LocalizedProperty> findByPropertyPropertyStatusAndLocale(PropertyStatus propertyStatus, String locale);
+
+	@Query("select lp from LocalizedProperty lp join fetch lp.property join fetch lp.property.imageSet.representativeImage " +
+		"where lp.property.propertyStatus = ?1 and lp.property.location = ?2 and lp.locale = ?3")
+	List<LocalizedProperty> findSimilar(PropertyStatus propertyStatus,
+		Location location, String locale, Pageable pageable);
 
 }
--- webapp/src/main/java/org/gotocy/controllers/PropertiesController.java
+++ webapp/src/main/java/org/gotocy/controllers/PropertiesController.java
@@ -1,19 +1,16 @@
 package org.gotocy.controllers;
 
-import org.gotocy.domain.*;
+import org.gotocy.domain.LocalizedProperty;
 import org.gotocy.repository.LocalizedPropertyRepository;
-import org.gotocy.repository.LocalizedPropertySpecificationRepository;
-import org.gotocy.repository.PropertyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.multiaction.NoSuchRequestHandlingMethodException;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 
@@ -24,69 +21,30 @@ import java.util.Locale;
 public class PropertiesController {
 
 	@Autowired
-	private PropertyRepository propertyRepository;
-
-	@Autowired
-	private LocalizedPropertyRepository localizedPropertyRepository;
-
-	@Autowired
-	private LocalizedPropertySpecificationRepository localizedPropertySpecificationRepository;
-
-	@RequestMapping("/properties/add")
-	@ResponseBody
-	public LocalizedProperty addProperty(@RequestParam(required = false) Long id,
-		@RequestParam(required = false, defaultValue = "LARNACA") Location location,
-		@RequestParam(required = false, defaultValue = "34.7071301") Double latitude,
-		@RequestParam(required = false, defaultValue = "33.022617399999945") Double longitude,
-		@RequestParam(required = false, defaultValue = "100") Integer price,
-		Locale locale)
-	{
-		Property p;
-		if (id == null) {
-			p = new Property();
-			p.setPrice(price);
-			p.setLocation(location);
-			p.setLatitude(latitude);
-			p.setLongitude(longitude);
-			p.setPropertyType(PropertyType.HOUSE);
-			p.setPropertyStatus(PropertyStatus.LONG_TERM);
-			propertyRepository.save(p);
-		} else {
-			p = propertyRepository.findOne(id);
-		}
-
-		LocalizedProperty lp = new LocalizedProperty();
-		lp.setLocale(locale.getLanguage());
-		lp.setTitle("LP#" + p.getId());
-		lp.setAddress("Property address");
-		lp.setDescription("Some description");
-		lp.setProperty(p);
-
-		List<LocalizedPropertySpecification> specifications = new ArrayList<>();
-		LocalizedPropertySpecification lps = new LocalizedPropertySpecification();
-		lps.setSpecification("spec #1");
-		specifications.add(lps);
-		lp.setSpecifications(specifications);
-
-		localizedPropertyRepository.save(lp);
-
-		return lp;
-	}
+	private LocalizedPropertyRepository repository;
 
 	@RequestMapping("/properties")
 	@ResponseBody
 	public Iterable<LocalizedProperty> list() {
-		return localizedPropertyRepository.findAll();
+		return repository.findAll();
 	}
 
 	@RequestMapping("/property/{id}")
 	public String get(Model model, @PathVariable("id") Long id, Locale locale) throws NoSuchRequestHandlingMethodException {
-		LocalizedProperty lp = localizedPropertyRepository.findByPropertyIdAndLocale(id, locale.getLanguage());
+		LocalizedProperty lp = repository.findProperty(id, locale.getLanguage());
 		// TODO: replace with custom exception
 		if (lp == null)
 			throw new NoSuchRequestHandlingMethodException("get", PropertiesController.class);
-		lp.setSpecifications(localizedPropertySpecificationRepository.findByLocalizedProperty(lp));
 		model.addAttribute(lp);
+
+		List<LocalizedProperty> similar = repository.findSimilar(lp.getProperty().getPropertyStatus(),
+			lp.getProperty().getLocation(), locale.getLanguage(), new PageRequest(0, 4));
+
+		// Remove self or last (if there was no self
+		if (!similar.remove(lp))
+			similar.remove(3);
+		model.addAttribute("similarLocalizedProperties", similar);
+
 		return "property/show";
 	}
 
