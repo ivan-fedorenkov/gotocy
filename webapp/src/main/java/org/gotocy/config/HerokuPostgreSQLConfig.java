f0335f11f312995231cfc781229ceab5468fb56b
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2015-08-10 23:03:38 +0300

    [GTC-28] Migrate to real database

--- webapp/src/main/java/org/gotocy/config/HerokuPostgreSQLConfig.java
+++ webapp/src/main/java/org/gotocy/config/HerokuPostgreSQLConfig.java
@@ -1,27 +1,27 @@
 package org.gotocy.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 import javax.sql.DataSource;
+import java.net.URISyntaxException;
 
 /**
  * @author ifedorenkov
  */
 @Configuration
 @Profile("heroku-postgresql")
-@EnableConfigurationProperties(HerokuPostgreSQLProperties.class)
 public class HerokuPostgreSQLConfig {
 
-	@Autowired
-	private HerokuPostgreSQLProperties properties;
+	@Bean
+	public HerokuPostgreSQLProperties herokuPostgreSQLProperties() throws URISyntaxException {
+		return new HerokuPostgreSQLProperties();
+	}
 
 	@Bean
-	public DataSource dataSource() {
+	public DataSource dataSource(HerokuPostgreSQLProperties properties) {
 		DataSourceBuilder factory = DataSourceBuilder
 			.create(properties.getClassLoader())
 			.driverClassName(properties.getDriverClassName())
--- webapp/src/main/java/org/gotocy/config/HerokuPostgreSQLProperties.java
+++ webapp/src/main/java/org/gotocy/config/HerokuPostgreSQLProperties.java
@@ -1,10 +1,6 @@
 package org.gotocy.config;
 
 import org.springframework.beans.factory.BeanClassLoaderAware;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
 
 import java.net.URI;
 import java.net.URISyntaxException;
@@ -12,8 +8,6 @@ import java.net.URISyntaxException;
 /**
  * @author ifedorenkov
  */
-@Component
-@Profile("heroku-postgresql")
 public class HerokuPostgreSQLProperties implements BeanClassLoaderAware {
 
 	private ClassLoader classLoader;
