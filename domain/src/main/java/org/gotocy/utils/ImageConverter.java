b577cb18bcd19d3004c3223b0e2072ff96a98b86
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2015-11-27 19:02:39 +0300

    [GTC-50 Code] cleanup and maintenance

---
+++ domain/src/main/java/org/gotocy/utils/ImageConverter.java
@@ -0,0 +1,78 @@
+package org.gotocy.utils;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.gotocy.domain.Image;
+import org.gotocy.domain.ImageSize;
+import org.im4java.core.ConvertCmd;
+import org.im4java.core.IM4JavaException;
+import org.im4java.core.IMOperation;
+import org.im4java.core.IMOps;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.EnumMap;
+import java.util.Optional;
+
+/**
+ * Converter of images. Requires write access to the TEMP directory.
+ *
+ * @author ifedorenkov
+ */
+public class ImageConverter {
+
+	private static final Log log = LogFactory.getLog(ImageConverter.class);
+
+	private static final String TEMP_FILES_PREFIX = "IMAGE_CONVERTER";
+	private static final String TEMP_FILES_SUFFIX = ".tmp";
+
+	private static final EnumMap<ImageSize, IMOps> RESIZE_OPS = new EnumMap<>(ImageSize.class);
+
+	static {
+		IMOperation resizeBigOp = new IMOperation();
+		resizeBigOp.addImage();
+		resizeBigOp.quality(70d).resize(1920, 1080, "^");
+		resizeBigOp.addImage();
+		RESIZE_OPS.put(ImageSize.BIG, resizeBigOp);
+
+		IMOperation resizeMediumOp = new IMOperation();
+		resizeMediumOp.addImage();
+		resizeMediumOp.quality(50d).resize(524, 394, "^").crop(524, 394, 0, 0);
+		resizeMediumOp.addImage();
+		RESIZE_OPS.put(ImageSize.MEDIUM, resizeMediumOp);
+	}
+
+	private ImageConverter() {
+	}
+
+	/**
+	 * Converts the given source {@link Image} to the specified {@link ImageSize} and creates
+	 * a new {@link Image} instance. Underlying bytes of the new image represents the converted image and the
+	 * key is set in accordance with {@link Image#getKeyForSize(ImageSize)} method.
+	 *
+	 * @param sourceImage to be converted
+	 * @param targetSize of the resulting image
+	 * @return the converted image or {@link Optional#EMPTY} if something went wrong.
+	 */
+	public static Optional<Image> convertToSize(Image sourceImage, ImageSize targetSize) {
+		log.info("Converting image [" + sourceImage.getKey() + "] to size [" + targetSize + "].");
+		Optional<Image> converted = Optional.empty();
+		try {
+			Path src = Files.createTempFile(TEMP_FILES_PREFIX, TEMP_FILES_SUFFIX);
+			Files.write(src, sourceImage.getBytes());
+			Path dst = Files.createTempFile(TEMP_FILES_PREFIX, TEMP_FILES_SUFFIX);
+
+			ConvertCmd cmd = new ConvertCmd();
+			cmd.run(RESIZE_OPS.get(targetSize), src.toString(), dst.toString());
+
+			Image convertedImage = new Image(sourceImage.getKeyForSize(targetSize));
+			convertedImage.setBytes(Files.readAllBytes(dst));
+			converted = Optional.of(convertedImage);
+		} catch (InterruptedException | IM4JavaException | IOException e) {
+			log.error("Failed to convert [" + sourceImage.getKey() + "] to size [" + targetSize + "].", e);
+		}
+		return converted;
+	}
+
+}
---
+++ domain/src/test/java/org/gotocy/utils/ImageConverterTest.java
@@ -0,0 +1,37 @@
+package org.gotocy.utils;
+
+import org.gotocy.domain.Image;
+import org.gotocy.domain.ImageSize;
+import org.junit.Assert;
+import org.junit.Test;
+import org.springframework.util.StreamUtils;
+
+import java.io.InputStream;
+import java.util.Optional;
+
+/**
+ * @author ifedorenkov
+ */
+public class ImageConverterTest {
+
+	@Test
+	public void testConvert() throws Exception {
+		Image original = new Image("cyprus.jpg");
+		try (InputStream is = getClass().getClassLoader().getResourceAsStream(original.getKey())) {
+			original.setBytes(StreamUtils.copyToByteArray(is));
+		}
+
+		// Check all image sizes so that if someone would add a new size the test should fail
+		for (ImageSize imageSize : ImageSize.values()) {
+			Image expected = new Image(original.getKeyForSize(imageSize));
+			try (InputStream is = getClass().getClassLoader().getResourceAsStream(expected.getKey())) {
+				expected.setBytes(StreamUtils.copyToByteArray(is));
+			}
+			Optional<Image> actual = ImageConverter.convertToSize(original, imageSize);
+			Assert.assertTrue(actual.isPresent());
+			Assert.assertEquals(expected.getKey(), actual.get().getKey());
+			Assert.assertArrayEquals(expected.getBytes(), actual.get().getBytes());
+		}
+	}
+
+}
