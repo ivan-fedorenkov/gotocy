bd1cc969188b9bf46887dd1246029bc5dcee021f
Ivan Fedorenkov <ivan.fedorenkov@gmail.com>
2016-03-20 20:25:46 +0300

    [GTC-85] Remove temp redirects after 01.02.2016

--- webapp/src/main/java/org/gotocy/filters/UrlRewriteFilter.java
+++ webapp/src/main/java/org/gotocy/filters/UrlRewriteFilter.java
@@ -22,13 +22,6 @@ public class UrlRewriteFilter extends OncePerRequestFilter {
 
 	private static final Pattern JSESSIONID_PATTERN =
 		Pattern.compile("^(.*?)(?:\\;jsessionid=[^\\?#]*)(\\?[^#]*)?(#.*)?$");
-	private static final Pattern HEROKU_DOMAIN_PATTERN =
-		Pattern.compile("^(.*?)(?:gotocy\\.herokuapp\\.com)(.*)$");
-
-	// TODO: remove these patterns after 01.02.2016 (3 months period for web crawlers)
-	private static final Pattern PROPERTY_PATTERN = Pattern.compile("^(.*?)(?:/property)(/.*)?$");
-	private static final Pattern COMPLEX_PATTERN = Pattern.compile("^(.*?)(?:/complex)(/.*)?$");
-	private static final Pattern DEVELOPERS_PATTERN = Pattern.compile("^(.*?)(?:/developer)(/.*)?$");
 
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
@@ -43,38 +36,6 @@ public class UrlRewriteFilter extends OncePerRequestFilter {
 				session.invalidate();
 			applyRedirect(response, valOrEmpty(jsessionidMatcher.group(1)) +
 				valOrEmpty(jsessionidMatcher.group(2)) + valOrEmpty(jsessionidMatcher.group(3)));
-			return;
-		}
-
-		Matcher herokuMatcher = HEROKU_DOMAIN_PATTERN.matcher(request.getRequestURL());
-
-		if (herokuMatcher.matches()) {
-			applyRedirect(response, valOrEmpty(herokuMatcher.group(1)) + "www.gotocy.eu" +
-				valOrEmpty(herokuMatcher.group(2)));
-			return;
-		}
-
-		Matcher propertyMatcher = PROPERTY_PATTERN.matcher(request.getRequestURL());
-
-		if (propertyMatcher.matches()) {
-			applyRedirect(response, valOrEmpty(propertyMatcher.group(1)) + "/properties" +
-				valOrEmpty(propertyMatcher.group(2)));
-			return;
-		}
-
-		Matcher complexMatcher = COMPLEX_PATTERN.matcher(request.getRequestURL());
-
-		if (complexMatcher.matches()) {
-			applyRedirect(response, valOrEmpty(complexMatcher.group(1)) + "/complexes" +
-				valOrEmpty(complexMatcher.group(2)));
-			return;
-		}
-
-		Matcher developerMatcher = DEVELOPERS_PATTERN.matcher(request.getRequestURL());
-
-		if (developerMatcher.matches()) {
-			applyRedirect(response, valOrEmpty(developerMatcher.group(1)) + "/developers" +
-				valOrEmpty(developerMatcher.group(2)));
 			return;
 		}
 
