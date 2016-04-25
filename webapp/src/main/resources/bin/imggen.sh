c10290eff415342fb27d68fccbaa20bb378a3354
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2016-04-25 13:47:51 +0300

    [GTC-50] Code cleanup and maintenance

--- webapp/src/main/resources/bin/imggen.sh
+++ webapp/src/main/resources/bin/imggen.sh
@@ -10,14 +10,6 @@ while true; do
 done
 
 echo
-echo "Cleaning up..."
-rm -rf MEDIUM*.jpg SMALL*.jpg THUMB*.jpg
-rm -rf MEDIUM BIG
-echo
-echo "OK"
-
-
-echo
 echo "JPG -> jpg ..."
 find . -name "*.JPG" -exec sh -c 'mv "${1}" "${1%.*}".jpg' _ {} \;
 echo
@@ -30,19 +22,14 @@ echo
 echo "OK"
 
 echo
-echo "Creating images of appropriate size..."
-mkdir MEDIUM BIG
-find . -maxdepth 1 -name "*.jpg" -exec sh -c 'convert -quality 50 -resize 524x394^ -gravity center -crop 524x394+0+0 +repage "${1}" "MEDIUM/${1##*/}"' _ {} \;
-find . -maxdepth 1 -name "*.jpg" -exec sh -c 'convert -quality 70 -resize 1920x1080^ -gravity center "${1}" "BIG/${1##*/}"' _ {} \;
+echo ""Resizing original images to the reasonable size..."
 find . -maxdepth 1 -name "*.jpg" -exec sh -c 'convert -quality 80 -resize 3840x2160^ "${1}" "${1}"' _ {} \;
 echo
 echo "OK"
 
 echo
 echo "Uploading images to S3..."
-aws s3 cp . s3://assets.gotocy.com/property/$ID/ --recursive --exclude "*" --include "*.jpg" --exclude "MEDIUM/*" --exclude "BIG/*"
-aws s3 cp MEDIUM/ s3://assets.gotocy.com/property/$ID/MEDIUM/ --recursive --storage-class REDUCED_REDUNDANCY
-aws s3 cp BIG/ s3://assets.gotocy.com/property/$ID/BIG/ --recursive --storage-class REDUCED_REDUNDANCY
+aws s3 cp . s3://assets.gotocy.com/property/$ID/ --storage-class STANDARD_IA --recursive --exclude "*" --include "*.jpg"
 echo
 echo "OK"
 
