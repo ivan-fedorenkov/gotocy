686625e8dab73c7ea9823ddcd57b5e0d74861dd1
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2016-07-13 12:55:13 +0300

    [GTC-50] Code cleanup and maintenance

--- webapp/src/main/java/org/gotocy/controllers/CommercialController.java
+++ webapp/src/main/java/org/gotocy/controllers/CommercialController.java
@@ -2,9 +2,12 @@ package org.gotocy.controllers;
 
 import org.gotocy.config.Locales;
 import org.gotocy.controllers.exceptions.NotFoundException;
+import org.gotocy.helpers.Helper;
+import org.gotocy.i18n.I18n;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Locale;
 
@@ -13,6 +16,22 @@ import java.util.Locale;
  */
 @Controller
 public class CommercialController {
+
+	@RequestMapping(value = "/gmdeco4u-ad-block", method = RequestMethod.GET, produces = "text/html")
+	@ResponseBody
+	public String adBlock() {
+		return "<div class=\"property small\">" +
+			"<a th:href=\"" + Helper.path("/gmdeco4u") + "\">" +
+			"<div class=\"property-image\">" +
+			"<img alt=\"G.M. Deco\" src=\"http://assets.gotocy.com/commercial/gm_deco/logo_side.jpg\">" +
+			"</div>" +
+			"</a>" +
+			"<div class=\"info\">" +
+			"<a th:href=\"" + Helper.path("/gmdeco4u") + "\"><h4>G.M. Deco For You</h4></a>" +
+			"<figure>" + I18n.t("templates.property.show.gm-deco-side-description") + "</figure>" +
+			"</div>" +
+			"</div>";
+	}
 
 	@RequestMapping(value = "/gmdeco4u", method = RequestMethod.GET)
 	public String get(Locale locale) {
