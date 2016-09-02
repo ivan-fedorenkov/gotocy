be5887ae7f0da80818c217bdfabb051286014855
Ivan Fedorenkov <ifedorenkov@devexperts.com>
2016-09-02 13:26:36 +0300

    [GTC-119] Personal accounts: property management

--- webapp/src/main/java/org/gotocy/forms/validation/UserRegistrationFormValidator.java
+++ webapp/src/main/java/org/gotocy/forms/validation/UserRegistrationFormValidator.java
@@ -11,6 +11,7 @@ import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
 import java.util.Objects;
+import java.util.regex.Pattern;
 
 /**
  * Validator of the {@link UserRegistrationForm}.
@@ -23,6 +24,14 @@ public class UserRegistrationFormValidator implements Validator {
 
 	public static final String NON_MATCHING_PASSWORDS =
 		"org.gotocy.validation.constraints.UserRegistrationFormValidator.NonMatchingPasswords.message";
+	public static final String INVALID_EMAIL =
+		"org.gotocy.validation.constraints.UserRegistrationFormValidator.InvalidEmail.message";
+	public static final String PASSWORD_TOO_SHORT =
+		"org.gotocy.validation.constraints.UserRegistrationFormValidator.PasswordTooShort.message";
+
+	private static final Pattern VALID_EMAIL_ADDRESS_REGEX =
+		Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE);
+	private static final int MINIMUM_PASSWORD_LENGTH = 5;
 
 	private final UserService userService;
 
@@ -43,12 +52,20 @@ public class UserRegistrationFormValidator implements Validator {
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "email", form.getEmail());
 		ValidationUtils.rejectIfEmptyOrWhitespace(errors, "password", form.getPassword());
 
-		if (!Objects.equals(form.getPassword(), form.getConfirmPassword()))
+		if (!errors.hasFieldErrors("email") && !VALID_EMAIL_ADDRESS_REGEX.matcher(form.getEmail()).matches())
+			errors.rejectValue("email", INVALID_EMAIL);
+
+		if (!errors.hasFieldErrors("password") && form.getPassword().length() < MINIMUM_PASSWORD_LENGTH)
+			errors.rejectValue("password", PASSWORD_TOO_SHORT, new Object[] {MINIMUM_PASSWORD_LENGTH}, null);
+
+		if (!errors.hasFieldErrors("password") && !Objects.equals(form.getPassword(), form.getConfirmPassword()))
 			errors.rejectValue("password", NON_MATCHING_PASSWORDS);
 
-		// Username must be unique
-		GtcUser existingUser = userService.findByUsername(form.getEmail());
-		if (existingUser != null)
-			errors.rejectValue("email", ValidationConstraints.NON_UNIQUE, null);
+		if (!errors.hasErrors()) {
+			// Username must be unique
+			GtcUser existingUser = userService.findByUsername(form.getEmail());
+			if (existingUser != null)
+				errors.rejectValue("email", ValidationConstraints.NON_UNIQUE, null);
+		}
 	}
 }
--- webapp/src/test/java/org/gotocy/forms/validation/UserRegistrationFormValidatorTest.java
+++ webapp/src/test/java/org/gotocy/forms/validation/UserRegistrationFormValidatorTest.java
@@ -18,6 +18,10 @@ public class UserRegistrationFormValidatorTest {
 	private static final String FORM_OBJECT_NAME = "userRegistrationForm";
 	private static final String EMPTY_STRING = "";
 	private static final String ANY_STRING = "any string";
+	private static final String ANY_VALID_EMAIL = "anything@mail.xyz";
+	private static final String INVALID_EMAIL = ANY_STRING;
+	private static final String ANY_VALID_PASSWORD = "at_least_five_symbols";
+	private static final String TOO_SHORT_PASSWORD = "1234";
 
 	@Test
 	public void testRejections() {
@@ -28,8 +32,11 @@ public class UserRegistrationFormValidatorTest {
 		UserRegistrationForm form = new UserRegistrationForm();
 		Errors errors = new BeanPropertyBindingResult(form, FORM_OBJECT_NAME);
 		formValidator.validate(form, errors);
+		Assert.assertEquals(1, errors.getFieldErrorCount("name"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("name").getCode());
+		Assert.assertEquals(1, errors.getFieldErrorCount("email"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("email").getCode());
+		Assert.assertEquals(1, errors.getFieldErrorCount("password"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("password").getCode());
 
 		form.setName(EMPTY_STRING);
@@ -37,22 +44,38 @@ public class UserRegistrationFormValidatorTest {
 		form.setPassword(EMPTY_STRING);
 		errors = new BeanPropertyBindingResult(form,  FORM_OBJECT_NAME);
 		formValidator.validate(form, errors);
+		Assert.assertEquals(1, errors.getFieldErrorCount("name"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("name").getCode());
+		Assert.assertEquals(1, errors.getFieldErrorCount("email"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("email").getCode());
+		Assert.assertEquals(1, errors.getFieldErrorCount("password"));
 		Assert.assertEquals(ValidationConstraints.NOT_EMPTY, errors.getFieldError("password").getCode());
 
 		form.setName(ANY_STRING);
-		form.setEmail(ANY_STRING);
-		form.setPassword(ANY_STRING);
+		form.setEmail(INVALID_EMAIL);
+		form.setPassword(TOO_SHORT_PASSWORD);
+		errors = new BeanPropertyBindingResult(form, FORM_OBJECT_NAME);
+		formValidator.validate(form, errors);
+		Assert.assertEquals(1, errors.getFieldErrorCount("email"));
+		Assert.assertEquals(UserRegistrationFormValidator.INVALID_EMAIL,
+			errors.getFieldError("email").getCode());
+		Assert.assertEquals(1, errors.getFieldErrorCount("password"));
+		Assert.assertEquals(UserRegistrationFormValidator.PASSWORD_TOO_SHORT,
+			errors.getFieldError("password").getCode());
+
+		form.setName(ANY_STRING);
+		form.setEmail(ANY_VALID_EMAIL);
+		form.setPassword(ANY_VALID_PASSWORD);
 		errors = new BeanPropertyBindingResult(form,  FORM_OBJECT_NAME);
 		formValidator.validate(form, errors);
+		Assert.assertEquals(1, errors.getFieldErrorCount("password"));
 		Assert.assertEquals(UserRegistrationFormValidator.NON_MATCHING_PASSWORDS,
 			errors.getFieldError("password").getCode());
 
 		form.setName(ANY_STRING);
-		form.setEmail(ANY_STRING);
-		form.setPassword(ANY_STRING);
-		form.setConfirmPassword(ANY_STRING);
+		form.setEmail(ANY_VALID_EMAIL);
+		form.setPassword(ANY_VALID_PASSWORD);
+		form.setConfirmPassword(ANY_VALID_PASSWORD);
 		errors = new BeanPropertyBindingResult(form, FORM_OBJECT_NAME);
 		formValidator.validate(form, errors);
 		Assert.assertFalse(errors.hasErrors());
@@ -66,9 +89,9 @@ public class UserRegistrationFormValidatorTest {
 
 		UserRegistrationForm form = new UserRegistrationForm();
 		form.setName(ANY_STRING);
-		form.setEmail(ANY_STRING);
-		form.setPassword(ANY_STRING);
-		form.setConfirmPassword(ANY_STRING);
+		form.setEmail(ANY_VALID_EMAIL);
+		form.setPassword(ANY_VALID_PASSWORD);
+		form.setConfirmPassword(ANY_VALID_PASSWORD);
 		Errors errors = new BeanPropertyBindingResult(form, FORM_OBJECT_NAME);
 		formValidator.validate(form, errors);
 		Assert.assertEquals(ValidationConstraints.NON_UNIQUE, errors.getFieldError("email").getCode());
